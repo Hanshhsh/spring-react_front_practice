26493d961e383e56dd4ce452a073634285d1aca7
Hanshhsh <killshot020542@gmail.com>
2023-02-18 00:34:42 +0900

    useEffect, axios통신 사용

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,32 +1,52 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = () =>{
-  //숫자 인풋, 저장 하는 코드
-  const [text, setText] = useState("11");
-  const [edit, setEdit] = useState(false);
+const App = () => {
+  const [posts, setPosts] = useState([]);
 
-  let content = <div>
-    {text}<button onClick={() => setEdit(true)}>수정</button></div>
+  useEffect(() => {
+    // axios({ //axios 사용 1번쨰 방법
+    //   method: 'GET',
+    //   url:'https://jsonplaceholder.typicode.com/photos'
+    // }).then(response => setPosts(response.data))
 
-  if(edit) {
-    content = <div><input type="text" value={text}
-    onChange={(e) => {
-      console.log(e.target.value);
-      setText(e.target.value);
-    }} />
-    <button onClick={() => setEdit(false)}>수정 </button>
-   </div>
-  }
-  return(
-    <>
-   {content}
-   </>
+    // axios.get('https://jsonplaceholder.typicode.com/photos')   //axios 사용 2번쨰 방법
+    //       .then(response => setPosts(response.data))
+
+    // const response = await axios.get('https://jsonplaceholder.typicode.com/photos');  //axios 사용 3번쨰 방법 (useEffect 선언 부에 async를 사용해야함. 그 방식 별로긴함)
+    // setPosts(response.data);
+    const fetchData = async () => {
+      //axios 사용 4번째 방법
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/photos"
+        );
+        setPosts(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  });
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <div>{post.title}</div>
+          <div>
+            <img src={post.thumbnailUrl} alt="colorView" />
+          </div>
+        </li>
+      ))}
+    </ul>
+
+    // <div>
+    //   안녕하세요
+    //   {posts.length}
+    // </div>
   );
-}
-
-
-
-
+};
 
 export default App;
