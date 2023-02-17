870320d56fb11f451e9d8a72b951c42b5a50b5a2
Hanshhsh <killshot020542@gmail.com>
2023-02-17 22:11:39 +0900

    ddddd

---
+++ reactfront/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React, {useState} from 'react';
+
+const App = () =>{
+  const [text, setText] = useState("11");
+  const [edit, setEdit] = useState(false);
+
+  let content = <div>
+    {text}<button onClick={() => setEdit(true)}>수정</button></div>
+
+  if(edit) {
+    content = <div><input type="text" value={text}
+    onChange={(e) => {
+      console.log(e.target.value);
+      setText(e.target.value);
+    }} />
+    <button onClick={() => setEdit(false)}>수정 </button>
+   </div>
+  }
+  return(
+    <>
+   {content}
+   </>
+  );
+}
+
+
+
+
+
+export default App;
